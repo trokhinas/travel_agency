e5acbe502d3c2fc68f9d57572fa4acd0b8c31575
tosha <toxatr@icloud.com>
2019-01-27 15:08:14 +0300

    added custom query to order repository

--- database/src/main/java/com/digital_league/Repository/TA_OrderRepository.java
+++ database/src/main/java/com/digital_league/Repository/TA_OrderRepository.java
@@ -1,8 +1,12 @@
 package com.digital_league.Repository;
 
+import com.digital_league.DTO.TA_OrderDTO;
 import com.digital_league.Entity.TA_OrderEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface TA_OrderRepository extends JpaRepository<TA_OrderEntity, Long> {
 
+    public List<TA_OrderEntity> findAllByUser_UserId(Long id);
 }
